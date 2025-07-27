d27b75a08d20c6ae461cd4a6f74bf16803d279f7
Sudershan Pothina <sudershanpothina@virginias-mbp.lan>
2025-07-27 09:43:50 -0500

    add stack navigation

--- tutorials_habit_tracker/app/_layout.tsx
+++ tutorials_habit_tracker/app/_layout.tsx
@@ -3,5 +3,10 @@ import '../global.css';
 import { Stack } from 'expo-router';
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: 'Home' }} />
+      <Stack.Screen name="login" options={{ title: 'Login' }} />
+    </Stack>
+  );
 }
--- tutorials_habit_tracker/app/index.tsx
+++ tutorials_habit_tracker/app/index.tsx
@@ -1,9 +1,14 @@
-import { Text, View } from 'react-native';
+import { View, Text } from 'react-native';
+import React from 'react';
+import { Link } from 'expo-router';
 
 export default function Home() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="">Test</Text>
+      <Text> Home</Text>
+      <Link href={'/login'} className="h-5 w-20 rounded bg-cyan-500 text-center">
+        Login
+      </Link>
     </View>
   );
 }
